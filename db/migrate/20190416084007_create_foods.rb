32cd72bba7740630e5c36467d7752e29e10e2727
kasumi8pon <all.splashing@gmail.com>
2019-04-17 22:37:06 +0900

    foodのCRUDを追加

---
+++ db/migrate/20190416084007_create_foods.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateFoods < ActiveRecord::Migration[5.2]
+  def change
+    create_table :foods do |t|
+      t.string :name, null: false
+      t.integer :amount, null: false
+
+      t.timestamps
+    end
+  end
+end
