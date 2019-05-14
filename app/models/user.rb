3509cd0824bd9c383f8ec5f745c0af4d8c116433
kasumi8pon <all.splashing@gmail.com>
2019-05-14 09:11:20 +0900

    グループの最後のユーザーを削除した際、グループとそれに属するカテゴリー・食材を削除する

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   belongs_to :user_group
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  after_destroy :user_group_destroy
+
+  private
+
+    def user_group_destroy
+      self.user_group.destroy! unless self.user_group.has_user?
+    end
 end
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,7 +2,11 @@
 
 class UserGroup < ApplicationRecord
   has_many :users
-  has_many :foods
-  has_many :food_categories, -> { order(position: :asc) }
-  has_many :invitations
+  has_many :foods, dependent: :destroy
+  has_many :food_categories, -> { order(position: :asc) }, dependent: :destroy
+  has_many :invitations, dependent: :destroy
+
+  def has_user?
+    !(self.users.size == 0)
+  end
 end
