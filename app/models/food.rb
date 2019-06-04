8c4ce5c71803bac029ed56add94eb5fce8dc8b81
kasumi8pon <all.splashing@gmail.com>
2019-06-05 00:43:27 +0900

    量をenoughにした後、to_buyをfalseにするコールバックを追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,10 +5,17 @@ class Food < ApplicationRecord
   belongs_to :user_group
   validates :name, presence: true
   validates :amount, presence: true
+  after_update :check_to_buy
 
   enum amount: {
     empty: 0,
     short: 1,
     enough: 2
           }
+  
+  private
+
+    def check_to_buy
+      self.update_columns(to_buy: false) if self.amount == "enough"
+    end
 end
