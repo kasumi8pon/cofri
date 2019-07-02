fad865e6c3a0fc82f71353ca56f3245d2a29313e
kasumi8pon <all.splashing@gmail.com>
2019-07-02 17:13:26 +0900

    Slack button の url を修正

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -2,7 +2,7 @@
 
 module InvitationsHelper
   def share_url(invitation)
-    "http://slackbutton.herokuapp.com/post/new?url=coFriで一緒に冷蔵庫の中身を管理しましょう\n" +
+    "https://slackbutton.herokuapp.com/post/new?url=coFriで一緒に冷蔵庫の中身を管理しましょう\n" +
     edit_invitation_url(invitation.id, token: invitation.invitation_token)
   end
 end
