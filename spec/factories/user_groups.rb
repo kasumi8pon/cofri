dd6ae8024beca19f7b9f6d92b33e4fbade5c2040
kasumi8pon <all.splashing@gmail.com>
2019-11-05 21:45:19 +0900

    user_groupのfactoryにwith_user traitを追加

--- spec/factories/user_groups.rb
+++ spec/factories/user_groups.rb
@@ -2,5 +2,10 @@
 
 FactoryBot.define do
   factory :user_group do
+    trait :with_user do
+      before(:create) do |user_group|
+        user_group.users << FactoryBot.build(:user)
+      end
+    end
   end
 end
