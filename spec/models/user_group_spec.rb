6edd2e2cac814476eacf5907b5dbe8364f53bd16
kasumi8pon <all.splashing@gmail.com>
2019-11-05 22:58:47 +0900

    improve model spec
    テスト対象のメソッドを明確にして、必要な条件を追加した

--- spec/models/user_group_spec.rb
+++ spec/models/user_group_spec.rb
@@ -3,17 +3,32 @@
 require "rails_helper"
 
 RSpec.describe UserGroup, type: :model do
-  let(:user_group) { FactoryBot.create(:user_group) }
+  describe "#save" do
+    subject { user_group.save }
+    let(:user_group) { FactoryBot.build(:user_group) }
 
-  it "一人もユーザーがいないグループは、 has_user? が false を返すこと" do
-    expect(user_group.has_user?).to eq false
+    context "新しいユーザーグループを作成した場合" do
+      it "デフォルトカテゴリーも一緒に作成されること" do
+        subject
+        expect(user_group.food_categories.count).to eq 9
+      end
+    end
   end
 
-  it "新しいグループがつくられたとき、デフォルトカテゴリーも一緒に作成されること" do
-    expect(user_group.food_categories.count).to eq 9
-  end
+  describe "#has_user?" do
+    subject { user_group.has_user? }
+    let(:user_group) { FactoryBot.create(:user_group, :with_user) }
 
-  it "グループを削除したとき、カテゴリーも削除されること" do
-    expect { user_group.destroy }.to change { user_group.food_categories.count }.by(-9)
+    context "グループにユーザーが属している場合" do
+      it { is_expected.to eq true }
+    end
+
+    context "グループに1人もユーザーが属していない場合" do
+      before do
+        user_group.users.delete_all
+      end
+
+      it { is_expected.to eq false }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,31 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it "グループに最後のユーザーを削除したとき、グループも消える" do
-    user = FactoryBot.create(:user)
-    expect { user.destroy }.to change { UserGroup.count }.by(-1)
+  describe "#destroy" do
+    subject { @user.destroy }
+
+    before do
+      @user = user
+    end
+
+    context "ユーザーが所属しているグループに他のユーザーがいる場合" do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        FactoryBot.create(:user, user_group: user.user_group)
+      end
+
+      it "グループは消えないこと" do
+        expect { subject }.to change(UserGroup, :count).by(0)
+      end
+    end
+
+    context "ユーザーが所属しているグループに他のユーザーがいない場合" do
+      let(:user) { FactoryBot.create(:user) }
+
+      it "グループも消えること" do
+        expect { subject }.to change(UserGroup, :count).by(-1)
+      end
+    end
   end
 end
